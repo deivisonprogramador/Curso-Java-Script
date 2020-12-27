b2ff6801045afbc4e7105a50d0713d07474bb36b
Deivison da Silva e Silva <75956247+deivisonprogramador@users.noreply.github.com>
2020-12-27 13:12:02 -0300

    ex008

---
+++ Exercícios/ex008.js
@@ -0,0 +1,9 @@
+let vel = 10
+
+console.log(`A velocidade do seu carro é ${vel} km/h.`)
+
+if (vel>60 ){
+    console.log('Você ultrapassou a velocidade permitida! Multado!')
+}
+
+console.log('Dirija sempre com cinto de segurança!')
