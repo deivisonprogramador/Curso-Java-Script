ace58082a420be80600ae9f5cc23d98ce25fd075
Deivison da Silva e Silva <75956247+deivisonprogramador@users.noreply.github.com>
2021-01-15 22:19:46 -0300

    Create funcao01.js

---
+++ Exercícios/funcao01.js
@@ -0,0 +1,17 @@
+function parimp(n){
+    
+    if (n%2==0){
+        return 'O número é par!'
+
+    }
+    else{
+        return 'O número é ímpar!'
+
+
+    }
+}
+
+let res = parimp(17)
+console.log(res)
+
+console.log(parimp(19))
